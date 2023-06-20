ab24569b7522ae6aadd013c5ab776af7e01dc169
Darchevil <yderneville@gmail.com>
2023-06-21 01:21:08 +0200

    interface player + import

--- game-scoring-source/src/app/six-qui-prend/six-qui-prend.component.ts
+++ game-scoring-source/src/app/six-qui-prend/six-qui-prend.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Player } from '../player';
 
 @Component({
   selector: 'app-six-qui-prend',
   templateUrl: './six-qui-prend.component.html',
-  styleUrls: ['./six-qui-prend.component.css']
+  styleUrls: ['./six-qui-prend.component.css'],
 })
 export class SixQuiPrendComponent {
-
+  player: Player = {
+    name: 'Yoann',
+    score: 0,
+  };
 }
